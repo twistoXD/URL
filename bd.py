e1544abc7be73a14b860036fb063190053077e12
twist <artem.volozhanin@mail.ru>
2021-11-30 20:55:27 +0500

    сокращатель

---
+++ bd.py
@@ -0,0 +1,137 @@
+import sqlite3
+
+
+
+def db_create():
+    try:
+        con = sqlite3.connect('database.db')
+        cursor = con.cursor()
+        cursor.execute("""CREATE TABLE IF NOT EXISTS users(
+            id INTEGER,
+            login TEXT NOT NULL,
+            password TEXT NOT NULL,
+            PRIMARY KEY(id AUTOINCREMENT));""")
+        con.commit()
+        cursor.execute("""CREATE TABLE IF NOT EXISTS links(
+            id INTEGER,
+            user_id INTEGER,
+            link TEXT NOT NULL,
+            PRIMARY KEY(id AUTOINCREMENT),
+            FOREIGN KEY(user_id) REFERENCES users(id));""")
+        con.commit()
+    except sqlite3.Error:
+        print("Ошибка создания бд")
+    finally:
+        con.close()
+
+
+def db_loginId(login):
+    try:
+        con = sqlite3.connect('database.db')
+        cursor = con.cursor()
+        user = cursor.execute("""SELECT login FROM users WHERE login = ?""",(login,)).fetchone()
+        if not user:
+            print("Вы можете зарегистрироваться")
+            return 0
+        else:
+            print("Такой логин уже существует")
+            return 1
+    except sqlite3.Error:
+        print("Ошибка в авторизации")
+    finally:
+        con.close()
+
+
+
+
+def db_reg_user(login, password):
+    try:
+        print("Регистрация")
+        con = sqlite3.connect('database.db')
+        cursor = con.cursor()
+        cursor.execute("""INSERT INTO users (login, password) VALUES(?, ?)""",(login, password,))
+        con.commit()
+        print("Вы успешно зарегистрировались")
+    except sqlite3.Error:
+        print("Ошибка регистрации")
+        print(login)
+    finally:
+        con.close()
+
+
+
+def db_auth(login, password):
+    try:
+        print("Авторизация")
+        con = sqlite3.connect('database.db')
+        cursor = con.cursor()
+        user = cursor.execute("""SELECT * FROM users WHERE login = ? AND password = ?""",(login, password,)).fetchall()
+        if len(user) == 0:
+            print("Неверный логин или пароль")
+            return 0
+        else:
+            print(user)
+            return 1
+    except sqlite3.Error:
+        print("Ошибка авторизации")
+    finally:
+        con.close()
+
+
+def getUserByLogin(login):
+    try:
+        con = sqlite3.connect('database.db')
+        cursor = con.cursor()
+        res = cursor.execute("""SELECT * FROM users WHERE login = ? LIMIT 1""",(login,)).fetchone()
+        if not res:
+            print('Такого пользователя не существует')
+            return False   
+        return res
+    except sqlite3.Error as e:
+        print('Ошибка получения данных из БД - getUserByLogin - ' + str(e))
+    return False
+
+
+def getUser(user_id):
+    try:
+        con = sqlite3.connect('database.db')
+        cursor = con.cursor()
+        res = cursor.execute("""SELECT * FROM users WHERE id = ? LIMIT 1""",(user_id,)).fetchone()
+        if not res:
+            print('Такого пользователя не существует')
+            return False   
+        return res
+    except sqlite3.Error as e:
+        print('Ошибка получения данных из БД - getUser - ' + str(e))
+    return False
+
+
+
+
+def db_getLinkPrivate(user_id):
+    try:
+        con = sqlite3.connect('database.db')
+        cursor = con.cursor()
+        links = cursor.execute("""SELECT * FROM links WHERE user_id = ?""",(user_id,)).fetchall()
+        con.commit()
+        print(links)
+        return links
+    except sqlite3.Error:
+        print("Ошибка вывода ссылки")
+    finally:
+        con.close()
+
+
+def db_getLinkPublic():
+    try:
+        con = sqlite3.connect('database.db')
+        cursor = con.cursor()
+        links = cursor.execute("""SELECT * FROM links""").fetchall()
+        con.commit()
+        print(links)
+        return links
+    except sqlite3.Error:
+        print("Ошибка вывода ссылки")
+    finally:
+        con.close()
+
---
+++ main.py
@@ -0,0 +1,110 @@
+from flask import Flask, render_template, request, url_for, flash
+from flask_login.utils import logout_user
+from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.utils import redirect
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from UserLogin import UserLogin
+
+
+import bd
+import pyshorteners
+import pyperclip
+
+
+bd.db_create()
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hgfg85thf1hr1th56t89h5fg1ht'
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    print('load_user')
+    return UserLogin().fromDB(user_id, bd)
+
+
+
+menu = [
+{'name': 'Регистрация', 'url': 'register'},
+{'name': 'Авторизация', 'url': 'login'},]
+
+
+@app.route('/', methods=['POST', 'GET']) 
+def index(): 
+    print(url_for('index'))
+    links = bd.db_getLinkPublic()
+    if request.method == 'POST':
+        url = request.form.get('link')
+        link = pyshorteners.Shortener().tinyurl.short(url)
+        pyperclip.copy(link)
+        flash(f'Ваша ссылка: {link}')
+    return render_template('index.html', links = links,  menu = menu) 
+
+
+@app.route('/register', methods=['POST', 'GET']) 
+def register(): 
+    print(url_for('register'))
+    if request.method == 'POST': 
+        if len(request.form['password']) >= 8 and len(request.form['login'])>0:
+            login = request.form['login'] 
+            password = request.form['password'] 
+            hash = generate_password_hash(password)
+            reg = bd.db_loginId(login)
+            if reg == 1:
+                flash("Такой логин уже есть!")
+            elif reg == 0:
+                bd.db_reg_user(login, hash)
+                flash("Вы зарегистрировались!")
+                return redirect(url_for('login'))
+        else:
+            flash('Пароль должен содержать не менее 8 символов')
+
+    return render_template('register.html', title='Регистрация', menu = menu) 
+
+@app.route('/login', methods=['POST', 'GET']) 
+def login(): 
+    if current_user.is_authenticated:
+        return redirect(url_for('office'))
+
+    print(url_for('login'))
+    if request.method == 'POST': 
+        user = bd.getUserByLogin(request.form['login']) 
+        if user and check_password_hash(user[2], request.form['password']):
+            userLogin = UserLogin().create(user)
+            rm = True if request.form.get('remainme') else False
+            login_user(userLogin, remember=rm)
+            return redirect(request.args.get('next') or url_for('office'))
+            
+        flash('Неверный логин или пароль')
+     
+    return render_template('login.html', title='Авторизация', menu = menu) 
+
+
+
+
+@app.route('/office',  methods=['POST', 'GET']) 
+@login_required
+def office(): 
+    try:
+        user = bd.getUser(current_user.get_id())
+        login = user[1]   
+        bd.db_short(user[0])
+        flash('')
+    except:
+         flash('')
+    return render_template('office.html', title='Личный кабинет', login = login, menu = menu) 
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Вы вышли из аккаунта')
+    return redirect(url_for('login'))
+
+
+if __name__ == "__main__": 
+    app.run(debug=True)
